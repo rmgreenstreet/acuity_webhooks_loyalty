6009ae6846ee28dbc1123cccaae97b622ad981aa
Robert Greenstreet <robertgreenstreet@gmail.com>
2024-08-13 15:10:17 -0500

    Somehow the catch route is being hit?

--- index.js
+++ index.js
@@ -121,7 +121,8 @@ const updatedPaymentRequestHandler = async (req, res, next) => {
 app.post("/payment_updated", quickResponse, asyncWrapper(updatedPaymentRequestHandler));
 
 app.all("*", (req, res) => {
-    res.send("This is not a valid endpoint");
+  console.log(req.body);
+  res.send("This is not a valid endpoint");
 });
 
 app.listen(process.env.PORT, () => {
